85b8153d35107c736c26c17c25062bdba1bb08f7
Scott Hillman <hillmanov@gmail.com>
2022-12-01 07:22:11 -0700

    Day 1 complete

--- day01/day01.go
+++ day01/day01.go
@@ -1,19 +1,40 @@
 package main
 
 import (
+	"adventofcode/utils"
 	"embed"
 	"fmt"
+	"sort"
 )
 
 //go:embed input.txt
 var f embed.FS
 
+type elf struct {
+	food []int
+}
+
+func (e elf) totalCalories() int {
+	return utils.SumOf(e.food)
+}
+
 func Part1() any {
-	return nil
+	elves := readInput()
+	maxCalories := 0
+	for _, e := range elves {
+		maxCalories = utils.Max(maxCalories, e.totalCalories())
+	}
+	return maxCalories
 }
 
 func Part2() any {
-	return nil
+	elves := readInput()
+
+	sort.Slice(elves, func(i, j int) bool {
+		return elves[i].totalCalories() > elves[j].totalCalories()
+	})
+
+	return utils.SumOf([]int{elves[0].totalCalories(), elves[1].totalCalories(), elves[2].totalCalories()})
 }
 
 func main() {
@@ -22,4 +43,21 @@ func main() {
 
 	fmt.Printf("Day 01: Part 1: = %+v\n", part1Solution)
 	fmt.Printf("Day 01: Part 2: = %+v\n", part2Solution)
+}
+
+func readInput() []elf {
+	lines, _ := utils.ReadLines(f, "input.txt")
+
+	elves := []elf{}
+	e := elf{}
+	for _, line := range lines {
+		if line == "" {
+			elves = append(elves, e)
+			e = elf{}
+			continue
+		}
+		e.food = append(e.food, utils.ParseInt(line))
+	}
+	elves = append(elves, e)
+	return elves
 }
