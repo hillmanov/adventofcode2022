304951f67a7bf2385b6e07fbbe02223e1ac08310
Scott Hillman <hillmanov@gmail.com>
2022-12-09 18:08:27 -0700

    Some cleanup and renaming

--- day09/day09.go
+++ day09/day09.go
@@ -11,7 +11,7 @@ import (
 //go:embed input.txt
 var f embed.FS
 
-type Motion struct {
+type Move struct {
 	Direction string
 	Steps     int
 }
@@ -43,65 +43,61 @@ func (p Position) Dist(o Position) float64 {
 func Part1() any {
 	movements := getInput()
 
-	ropeLength := 2
-	ropePositions := simulateRope(movements, ropeLength)
+	knotCount := 2
+	knotPositions := simulateRope(movements, knotCount)
 
-	return len(ropePositions[ropeLength-1])
+	return len(knotPositions[knotCount-1])
 }
 
 func Part2() any {
 	movements := getInput()
 
-	ropeLength := 10
-	ropePositions := simulateRope(movements, ropeLength)
+	knotCount := 10
+	knotPositions := simulateRope(movements, knotCount)
 
-	return len(ropePositions[ropeLength-1])
+	return len(knotPositions[knotCount-1])
 }
 
-func simulateRope(movements []Motion, ropeLength int) []map[Position]bool {
+func simulateRope(moves []Move, knotCount int) []map[Position]bool {
 	HEAD := 0
 
-	// Create the rope and the positions
-	rope := make([]*Position, ropeLength)
-	ropePositions := make([]map[Position]bool, ropeLength)
-	for i := 0; i < ropeLength; i++ {
-		rope[i] = &Position{0, 0}
-		ropePositions[i] = map[Position]bool{*rope[i]: true}
+	// Create the knots and the positions
+	knots := make([]*Position, knotCount)
+	knotPositions := make([]map[Position]bool, knotCount)
+	for i := 0; i < knotCount; i++ {
+		knots[i] = &Position{0, 0}
+		knotPositions[i] = map[Position]bool{*knots[i]: true}
 	}
 
-	for _, movement := range movements {
-		for i := 0; i < movement.Steps; i++ {
-			for segment := 0; segment < ropeLength-1; segment++ {
+	for _, move := range moves {
+		for i := 0; i < move.Steps; i++ {
+			switch move.Direction {
+			case "U":
+				knots[HEAD].Move(Delta{0, 1})
+			case "D":
+				knots[HEAD].Move(Delta{0, -1})
+			case "L":
+				knots[HEAD].Move(Delta{1, 0})
+			case "R":
+				knots[HEAD].Move(Delta{-1, 0})
+			}
+			knotPositions[HEAD][*knots[HEAD]] = true
 
-				// Head and tail are relative to each other in the rope segment
-				headPosition := rope[segment]
-				tailPosition := rope[segment+1]
-
-				if segment == HEAD { // Movements apply to the head only
-					switch movement.Direction {
-					case "U":
-						headPosition.Move(Delta{0, 1})
-					case "D":
-						headPosition.Move(Delta{0, -1})
-					case "L":
-						headPosition.Move(Delta{1, 0})
-					case "R":
-						headPosition.Move(Delta{-1, 0})
-					}
-					ropePositions[segment][*headPosition] = true
-				}
+			for knot := 0; knot < knotCount-1; knot++ {
+				currentKnotPosition := knots[knot]
+				nextKnotPosition := knots[knot+1]
 
 				// Now check to see where the tail is relative to the head position and move accordingly, but only if we are far enough away.
-				if tailPosition.Dist(*headPosition) >= 2 {
-					tailPosition.Move(tailPosition.DeltaDirection(*headPosition))
+				if nextKnotPosition.Dist(*currentKnotPosition) >= 2 {
+					nextKnotPosition.Move(nextKnotPosition.DeltaDirection(*currentKnotPosition))
 				}
-				ropePositions[segment+1][*tailPosition] = true
+				knotPositions[knot+1][*nextKnotPosition] = true
 			}
 
 		}
 	}
 
-	return ropePositions
+	return knotPositions
 }
 
 func main() {
@@ -112,12 +108,12 @@ func main() {
 	fmt.Printf("Day 09: Part 2: = %+v\n", part2Solution)
 }
 
-func getInput() []Motion {
+func getInput() []Move {
 	lines, _ := utils.ReadLines(f, "input.txt")
-	movements := []Motion{}
+	movements := []Move{}
 
 	for _, line := range lines {
-		movements = append(movements, Motion{
+		movements = append(movements, Move{
 			Direction: string(line[0]),
 			Steps:     utils.ParseInt(strings.Trim(string(line[1:]), " ")),
 		})
