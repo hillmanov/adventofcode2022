785da120aafc8c0f6778c01b8fdc15e5d3b5bc56
Scott Hillman <hillmanov@gmail.com>
2022-12-20 08:13:34 -0700

    Day 18 complete!

--- day18/day18.go
+++ day18/day18.go
@@ -26,21 +26,36 @@ type Location struct {
 }
 
 type Cube struct {
-	X, Y, Z int
-	Exposed uint
+	X, Y, Z        int
+	Exposed        uint
+	ExposedToWater uint
 }
 
 func (c *Cube) Init() {
 	c.Exposed = TOP | BOTTOM | LEFT | RIGHT | FRONT | BACK
+	c.ExposedToWater = 0
 }
 
 func (c *Cube) Cover(side uint) {
 	c.Exposed &= ^side
 }
 
+func (c *Cube) CoverWithWater(side uint) {
+	c.ExposedToWater |= side
+}
+
 func (c Cube) ExposedSides() int {
 	return bits.OnesCount(c.Exposed)
 }
+
+func (c Cube) ExposedSidesToWater() int {
+	return bits.OnesCount(c.ExposedToWater)
+}
+
+const (
+	OBSIDIAN = 1
+	WATER    = 2
+)
 
 type Container struct {
 	MinX   int
@@ -49,19 +64,7 @@ type Container struct {
 	MaxY   int
 	MinZ   int
 	MaxZ   int
-	Filled map[Location]bool
-}
-
-func (c Container) ExpandedVolume() int {
-	return (c.MaxX - c.MinX) * (c.MaxY - c.MinY) * (c.MaxZ - c.MinZ)
-}
-
-func (c Container) InnerVolume() int {
-	return ((c.MaxX - c.MinX) - 2) * ((c.MaxY - c.MinY) - 2) * ((c.MaxZ - c.MinZ) - 2)
-}
-
-func (c Container) GetVolumeDiff() int {
-	return c.ExpandedVolume() - c.InnerVolume()
+	Filled map[Location]int
 }
 
 type Obsidian map[Location]*Cube
@@ -85,86 +88,8 @@ func GetSharedSides(c1, c2 *Cube) (uint, uint) {
 }
 
 func Part1() any {
-	cubes := getInput()
-	return getTotalSurfaceArea(cubes)
-}
-
-func Part2() any {
 	obsidian := getInput()
 
-	minX, maxX, minY, maxY, minZ, maxZ := math.MaxInt, math.MinInt, math.MaxInt, math.MinInt, math.MaxInt, math.MinInt
-
-	for location := range obsidian {
-		minX = utils.Min(minX, location.X)
-		maxX = utils.Max(maxX, location.X)
-		minY = utils.Min(minY, location.Y)
-		maxY = utils.Max(maxY, location.Y)
-		minZ = utils.Min(minZ, location.Z)
-		maxZ = utils.Max(maxZ, location.Z)
-	}
-
-	fmt.Printf("minX: %+v, maxX: %+v, minY: %+v, maxY: %+v, minZ: %+v, maxZ: %+v\n", minX, maxX, minY, maxY, minZ, maxZ)
-
-	container := Container{
-		MinX:   minX - 1,
-		MaxX:   maxX + 1,
-		MinY:   minY - 1,
-		MaxY:   maxY + 1,
-		MinZ:   minZ - 1,
-		MaxZ:   maxZ + 1,
-		Filled: make(map[Location]bool),
-	}
-
-	for location := range obsidian {
-		container.Filled[location] = true
-	}
-
-	var currentLocation Location
-findStartingLocation:
-	for x := minX; x <= maxX; x++ {
-		for y := minY; y <= maxY; y++ {
-			for z := minZ; z <= maxZ; z++ {
-				if _, ok := obsidian[Location{x, y, z}]; !ok {
-					currentLocation = Location{x, y, z}
-					break findStartingLocation
-				}
-			}
-		}
-	}
-
-	fmt.Printf("startingLocation = %+v\n", currentLocation)
-
-	volume := spread(currentLocation, container) - container.GetVolumeDiff()
-	fmt.Printf("volume = %+v\n", volume)
-
-	fmt.Printf("container.ExpandedVolume() = %+v\n", container.ExpandedVolume())
-	fmt.Printf("container.InnerVolumne() = %+v\n", container.InnerVolume())
-
-	totalSurfaceArea := getTotalSurfaceArea(obsidian)
-
-	// NOT 3250 - that is too high, 2008 is too low
-	return container.InnerVolume() - totalSurfaceArea
-}
-
-func spread(location Location, container Container) int {
-	if location.X < container.MinX || location.X > container.MaxX || location.Y < container.MinY || location.Y > container.MaxY || location.Z < container.MinZ || location.Z > container.MaxZ {
-		return 0
-	}
-	if _, ok := container.Filled[location]; ok {
-		return 0
-	}
-	container.Filled[location] = true
-	right := spread(Location{location.X + 1, location.Y, location.Z}, container)
-	left := spread(Location{location.X - 1, location.Y, location.Z}, container)
-	up := spread(Location{location.X, location.Y + 1, location.Z}, container)
-	down := spread(Location{location.X, location.Y - 1, location.Z}, container)
-	front := spread(Location{location.X, location.Y, location.Z + 1}, container)
-	back := spread(Location{location.X, location.Y, location.Z - 1}, container)
-
-	return 1 + right + left + up + down + front + back
-}
-
-func getTotalSurfaceArea(obsidian Obsidian) int {
 	for location, cube := range obsidian {
 		if _, ok := obsidian[Location{X: location.X + 1, Y: location.Y, Z: location.Z}]; ok {
 			cube.Cover(RIGHT)
@@ -192,6 +117,90 @@ func getTotalSurfaceArea(obsidian Obsidian) int {
 	}
 
 	return surfaceArea
+}
+
+func Part2() any {
+	obsidian := getInput()
+
+	minX, maxX, minY, maxY, minZ, maxZ := math.MaxInt, math.MinInt, math.MaxInt, math.MinInt, math.MaxInt, math.MinInt
+
+	for location := range obsidian {
+		minX = utils.Min(minX, location.X)
+		maxX = utils.Max(maxX, location.X)
+		minY = utils.Min(minY, location.Y)
+		maxY = utils.Max(maxY, location.Y)
+		minZ = utils.Min(minZ, location.Z)
+		maxZ = utils.Max(maxZ, location.Z)
+	}
+
+	container := Container{
+		MinX:   minX - 1,
+		MaxX:   maxX + 1,
+		MinY:   minY - 1,
+		MaxY:   maxY + 1,
+		MinZ:   minZ - 1,
+		MaxZ:   maxZ + 1,
+		Filled: make(map[Location]int),
+	}
+
+	for location := range obsidian {
+		container.Filled[location] = OBSIDIAN
+	}
+
+	currentLocation := Location{container.MinX, container.MinY, container.MinZ}
+
+	spread(currentLocation, container)
+
+	// Now go through each cube in the obsidian and see which ones are exposed to water, and mark the sides.
+	for _, cube := range obsidian {
+		// Check to see if cube shares the left side with a water cube in the container
+		if container.Filled[Location{X: cube.X - 1, Y: cube.Y, Z: cube.Z}] == WATER {
+			cube.CoverWithWater(LEFT)
+		}
+		if container.Filled[Location{X: cube.X + 1, Y: cube.Y, Z: cube.Z}] == WATER {
+			cube.CoverWithWater(RIGHT)
+		}
+		if container.Filled[Location{X: cube.X, Y: cube.Y - 1, Z: cube.Z}] == WATER {
+			cube.CoverWithWater(BOTTOM)
+		}
+		if container.Filled[Location{X: cube.X, Y: cube.Y + 1, Z: cube.Z}] == WATER {
+			cube.CoverWithWater(TOP)
+		}
+		if container.Filled[Location{X: cube.X, Y: cube.Y, Z: cube.Z - 1}] == WATER {
+			cube.CoverWithWater(BACK)
+		}
+		if container.Filled[Location{X: cube.X, Y: cube.Y, Z: cube.Z + 1}] == WATER {
+			cube.CoverWithWater(FRONT)
+		}
+	}
+
+	externalSurfaceArea := 0
+	for _, cube := range obsidian {
+		externalSurfaceArea += cube.ExposedSidesToWater()
+	}
+
+	return externalSurfaceArea
+}
+
+func spread(location Location, container Container) int {
+	if location.X < container.MinX || location.X > container.MaxX || location.Y < container.MinY || location.Y > container.MaxY || location.Z < container.MinZ || location.Z > container.MaxZ {
+		return 0
+	}
+
+	if _, ok := container.Filled[location]; ok {
+		return 0
+	}
+
+	container.Filled[location] = WATER
+
+	right := spread(Location{X: location.X + 1, Y: location.Y, Z: location.Z}, container)
+	left := spread(Location{X: location.X - 1, Y: location.Y, Z: location.Z}, container)
+	up := spread(Location{X: location.X, Y: location.Y + 1, Z: location.Z}, container)
+	down := spread(Location{X: location.X, Y: location.Y - 1, Z: location.Z}, container)
+	front := spread(Location{X: location.X, Y: location.Y, Z: location.Z + 1}, container)
+	back := spread(Location{X: location.X, Y: location.Y, Z: location.Z - 1}, container)
+
+	return 1 + right + left + up + down + front + back
 }
 
 func main() {
